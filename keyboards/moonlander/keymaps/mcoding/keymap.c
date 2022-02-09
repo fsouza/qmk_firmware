b86a47d5f2774314942b871cf41834b909dcfc74
francisco souza <108725+fsouza@users.noreply.github.com>
2022-02-09 11:06:24 -0500

    Add another mapping for LCTRL

--- keyboards/moonlander/keymaps/mcoding/keymap.c
+++ keyboards/moonlander/keymaps/mcoding/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
         KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
         KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
-        KC_LCTRL,       KC_LALT,        KC_LALT,        KC_LALT,        KC_LGUI,        KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_LBRACKET,    KC_RBRACKET,    KC_RALT,        KC_RALT,        MO(1),
+        KC_LCTRL,       KC_LCTRL,       KC_LALT,        KC_LALT,        KC_LGUI,        KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_LBRACKET,    KC_RBRACKET,    KC_RALT,        KC_RALT,        MO(1),
         KC_SPACE,       KC_BSPACE,      KC_LALT,                        KC_SPACE,       KC_BSPACE,      KC_ENTER
     ),
 
