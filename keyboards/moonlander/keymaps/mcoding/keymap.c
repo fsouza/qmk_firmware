acc7e252b6dc7bc927277f9f30f360f308963e1d
francisco souza <108725+fsouza@users.noreply.github.com>
2022-08-22 17:27:37 -0400

    mcoding: fix right thumb cluster
    I'm an idiot.

--- keyboards/moonlander/keymaps/mcoding/keymap.c
+++ keyboards/moonlander/keymaps/mcoding/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
         KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
         KC_LCTRL,       KC_LCTRL,       KC_LALT,        KC_LALT,        KC_LGUI,        KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_LBRACKET,    KC_RBRACKET,    KC_RALT,        KC_RALT,        MO(1),
-        KC_SPACE,       KC_BSPACE,      KC_TRANSPARENT,                 KC_SPACE,       KC_ENTER,       KC_TRANSPARENT
+        KC_SPACE,       KC_BSPACE,      KC_TRANSPARENT,                 KC_TRANSPARENT, KC_SPACE,       KC_ENTER
     ),
 
     [1] = LAYOUT_moonlander(
