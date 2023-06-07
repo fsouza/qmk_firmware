1273d78d329a8e868820dc2c2ee30c7b26ec096f
francisco souza <108725+fsouza@users.noreply.github.com>
2023-06-07 12:06:35 -0400

    Fix setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 MY_DIR=$(cd "$(dirname "${0}")" && pwd -P)
 GIT_DIR=${MY_DIR}/qmk_firmware
 QMK_REPO=zsa/qmk_firmware
-GIT_REPO=https://github.com/zsa/${QMK_REPO}.git
+GIT_REPO=https://github.com/${QMK_REPO}.git
 GIT_BRANCH=firmware22
 
 function ensure_git_repo() {
