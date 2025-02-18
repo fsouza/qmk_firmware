098a5afbb007121031653030056b090bd5fa8f69
francisco souza <108725+fsouza@users.noreply.github.com>
2025-02-18 19:41:49 +0000

    mcoding: update for firmware24

--- mcoding/keymap.c
+++ mcoding/keymap.c
@@ -1,25 +1,7 @@
 #include QMK_KEYBOARD_H
-#include "keymap_bepo.h"
-#include "keymap_contributions.h"
-#include "keymap_us_international.h"
 #include "version.h"
-
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define ES_LESS_MAC KC_GRAVE
-#define ES_GRTR_MAC LSFT(KC_GRAVE)
-#define ES_BSLS_MAC ALGR(KC_6)
-#define NO_PIPE_ALT KC_GRAVE
-#define NO_BSLS_ALT KC_EQUAL
-#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
-#define BP_NDSH_MAC ALGR(KC_8)
 #define MOON_LED_LEVEL LED_LEVEL
+#define ML_SAFE_RANGE SAFE_RANGE
 
 enum custom_keycodes {
     RGB_SLD = ML_SAFE_RANGE,
